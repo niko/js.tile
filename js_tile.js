7a6ab6bb676eeb85235aa82a0050fad2d0f29534
Niko Dittmann <niko@restorm.com>
2008-06-01 12:52:30 +0200

    first commit

---
+++ js_tile.js
@@ -0,0 +1,131 @@
+// Textile namespace
+//
+var Textile = Textile || {};
+
+// converter
+//
+// Wraps all "globals" so that the only thing
+// exposed is makeHtml().
+//
+Textile.converter = function() {
+  
+  this.to_html = function(text) {
+    text = this.empty_blank_lines(text);
+    text = this.quick_tags(text);
+    text = this.hyperref_images(text);
+    text = this.hyperrefs(text);
+    text = this.images(text);
+    text = this.headings(text);
+    text = this.unordered_lists(text);
+    return text;
+  };
+  
+  this.headings = function(text){
+    var header_re = new RegExp('h([1|2|3|4|5|6])\. ([^]+)\n\n');
+    return( text.replace(header_re,'<h$1>$2</h$1>\n') );
+  };
+  
+  this.empty_blank_lines = function(text){
+    return( text.replace( /\n\s*\n/g , '\n\n') );
+  };
+  
+  this.quick_tags = function(text){
+    // we want the words tightly surounded by the tags so things like 3 * 4  or 4 - 5 is not interpreted as tag.
+    var qtags =
+    [['\\*', 'strong'],
+    ['_', 'em'],
+    ['-', 'del'],
+    ['\\?\\?', 'cite'],
+    ['\\+', 'ins'],
+    ['@', 'code']];
+    for (var i=0; i<qtags.length; i++) {
+      ttag = qtags[i][0]; htag = qtags[i][1];
+      qt_re = new RegExp( '(^|\\s)' + ttag + '(.+?)' + ttag + '(\\s|,|!|\\?|\\.|$)' , 'g');
+      text = text.replace(qt_re,'$1<'+htag+'>'+'$2'+'</'+htag+'>$3');
+    }
+    // sup and sup tags may follow directly on a word character:
+    var qtags =
+    [['~', 'sub'],
+    ['\\^', 'sup']];
+    for (var i=0; i<qtags.length; i++) {
+      ttag = qtags[i][0]; htag = qtags[i][1];
+      qt_re = new RegExp( ttag + '(.+?)' + ttag + '(\\s|,|!|\\?|\\.|$)' , 'g');
+      text = text.replace(qt_re,'<'+htag+'>'+'$1'+'</'+htag+'>$2');
+    }
+    return(text);
+  }
+  
+  this.images = function(text){
+    img_re = new RegExp('!(.+?)!','g');
+    return( text.replace(img_re,'<img src="$1">') );
+  }
+  
+  link_goal = '([^\\s\\\\]+[^;.:?!,\\s\\\\])\\\\?([;.:?!,]?)'
+  this.hyperrefs = function(text){
+    var link_re = new RegExp('"(.+?)":' + link_goal,'g');
+    return( text.replace(link_re,'<a href="$2">$1</a>$3') );
+  }
+  
+  this.hyperref_images = function(text){
+    var href_link_re = new RegExp('!(.+?)!:' + link_goal,'g');
+    return( text.replace(href_link_re,'<a href="$2"><img src="$1"></a>$3') );
+  }
+  
+  this.unordered_lists = function(text){
+    var first_ul_re         = new RegExp('(^|\n\n)\\*(\\*)? (.+)\n','g');
+    var last_ul_re          = new RegExp('(\n)\\*(\\*)? (.+)\n(\n|$)','g');
+    var ul_re               = new RegExp('(\n)\\*(\\*)? (.+)','g');
+    var first_nested_ul_re   = new RegExp('(\n<li>.*)</li>\n\\*','g');
+    var last_nested_ul_re   = new RegExp('\n\\*(<li>.*</li>)(\n[^\\*])','g');
+    var middle_nester_re     = new RegExp('\n\\*(<li>)','g');
+    text = text.replace(first_ul_re,              '$1<ul>\n<li>$3</li>\n');
+    text = text.replace(last_ul_re,               '$1$2<li>$3</li>\n</ul>\n$4');
+    text = text.replace(ul_re,                    '$1$2<li>$3</li>');
+    text = text.replace(first_nested_ul_re,        '$1\n<ul>\n','g');
+    text = text.replace(last_nested_ul_re,        '\n$1\n</ul>\n</li>$2','g');
+    text = text.replace(middle_nester_re,          '\n$1','g');
+    return(text)
+  }
+  
+};
+
+function run_tests(){
+  var inputs = getElementsByClassNames('input');
+  var outputs = getElementsByClassNames('output');
+  var num_pending = getElementsByClassNames('input pending').length;
+  
+  if(inputs.length != outputs.length){ alert('we need the same number of inputs as outputs!'); }
+  
+  var converter = new Textile.converter();
+  var num_failing = 0;
+  for (var i = 0; i < inputs.length; i++){
+    converted = converter.to_html(inputs[i].innerHTML);
+    if ( converted != outputs[i].innerHTML ) {
+      inputs[i].style.backgroundColor = 'red';
+      outputs[i].style.backgroundColor = 'red';
+      outputs[i].innerHTML = converted + '<hr/>' + outputs[i].innerHTML;
+      num_failing++;
+    }else{
+      inputs[i].style.backgroundColor = 'green';
+      outputs[i].style.backgroundColor = 'green';
+    }
+  }
+  alert('total: ' + inputs.length + '; failing: ' + num_failing + '; pending: ' + num_pending);
+}
+
+// returns all elements with exactly matching classnames.
+function getElementsByClassNames(classnames){
+  var elements = [];
+  var allElems = document.getElementsByTagName('*');
+  for (var i = 0; i < allElems.length; i++) {
+    var thisElem = allElems[i];
+    if (thisElem.className && thisElem.className == classnames) {
+      elements.push(thisElem);
+    }
+  }
+  return(elements);
+}
+
+
+
+window.onload = run_tests; 
